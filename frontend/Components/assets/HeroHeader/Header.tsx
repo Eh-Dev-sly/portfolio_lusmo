10fb2d5f27f63521933378c0dbf7c3553df3e8ad
Ehouarn-Nouelig Sioly <eh.dev.sioly@gmail.com>
2025-11-12 15:42:32 +0100

    feat: add IntroAnimation with conditional Header display

--- frontend/Components/assets/HeroHeader/Header.tsx
+++ frontend/Components/assets/HeroHeader/Header.tsx
@@ -1,15 +1,7 @@
-// src/components/HeroSection.tsx
+import "@/Components/assets/HeroHeader/Header.scss"
+
 export default function HeroSection() {
   return (
-    <section
-      id="hero"
-      className="min-h-screen flex flex-col justify-center items-center text-center px-6"
-    >
-      <h1 className="text-5xl font-bold mb-4">Bienvenue sur mon portfolio</h1>
-      <p className="text-lg max-w-xl text-gray-600">
-        Découvrez mes projets, mon parcours et mes compétences à travers une
-        expérience claire et moderne.
-      </p>
-    </section>
+    <h1>Hey</h1>
   );
 }
