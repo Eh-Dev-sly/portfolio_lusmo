5e8021d98abd8b21c5e1398afc30f26776455dcd
Ehouarn-Nouelig Sioly <eh.dev.sioly@gmail.com>
2025-11-13 11:28:04 +0100

    style: center header text on the homepage

--- frontend/Components/assets/HeroHeader/Header.tsx
+++ frontend/Components/assets/HeroHeader/Header.tsx
@@ -2,6 +2,13 @@ import "@/Components/assets/HeroHeader/Header.scss"
 
 export default function HeroSection() {
   return (
-    <h1>Hey</h1>
+    <section className="home">
+      <div className="intro-wrapper">
+      <div className="intro_text">
+        <h1 className="text_title"> Hey, je m’appelle <div className="name">Ehouarn Sioly</div></h1>
+        <h2 className="text_title"> Mais vous pouvez m’appeler <div className="name">Lusmo</div></h2>
+      </div>
+      </div>
+    </section>
   );
 }
